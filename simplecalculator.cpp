4ee45f2e223c6b45c250afdcf484085666d28b64
daksh-web <dakshyd13@gmail.com>
2025-08-04 20:13:13 +0530

    Create simplecalculator.cpp

---
+++ simplecalculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double number1, number2;
+    char operation;
+
+    cout << "      SIMPLE CALCULATOR       \n";
+    
+    cout << "Enter the first number: ";
+    cin >> number1;
+
+    cout << "Enter the second number: ";
+    cin >> number2;
+
+
+    cout << "\nSelect an operation to perform (+, -, *, /): ";
+    cin >> operation;
+
+    
+    cout << "\nResult: ";
+    switch (operation) {
+        case '+':
+            cout << number1 << " + " << number2 << " = " << (number1 + number2);
+            break;
+        case '-':
+            cout << number1 << " - " << number2 << " = " << (number1 - number2);
+            break;
+        case '*':
+            cout << number1 << " * " << number2 << " = " << (number1 * number2);
+            break;
+        case '/':
+            if (number2 != 0)
+                cout << number1 << " / " << number2 << " = " << (number1 / number2);
+            else
+                cout << "Error: Division by zero is not allowed.";
+            break;
+        default:
+            cout << "Invalid operation. Please choose one of +, -, *, /.";
+    }
+
+    cout << "\n\nThank you for using the calculator!\n";
+    return 0;
+}
